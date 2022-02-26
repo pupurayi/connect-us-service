e48c3582c81b12eded8398bd75d0b4ced784cd74
Victor Chinyavada <vchinyavada@zss.co.zw>
2022-02-26 18:02:52 +0200

    first commit

---
+++ src/main/java/zw/co/connectus/ConnectUsServiceApplication.java
@@ -0,0 +1,14 @@
+package zw.co.connectus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectUsServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ConnectUsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zw/co/connectus/ConnectUsServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package zw.co.connectus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectUsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
