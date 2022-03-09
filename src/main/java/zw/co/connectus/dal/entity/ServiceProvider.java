13e23b9f73a0c4702e900c7a580739be6e0c4838
Victor Chinyavada <vchinyavada@zss.co.zw>
2022-03-09 10:36:15 +0200

    ServiceProvider entity add fields

--- src/main/java/zw/co/connectus/dal/entity/ServiceProvider.java
+++ src/main/java/zw/co/connectus/dal/entity/ServiceProvider.java
@@ -10,5 +10,48 @@ import javax.persistence.Table;
 @Table
 @Where(clause = "DELETED IS NULL")
 public class ServiceProvider extends JpaBaseEntity {
+	private String name;
+	private String address;
+	private double lat;
+	private double lng;
 
+	public String getName() {
+
+		return name;
+	}
+
+	public void setName(String name) {
+
+		this.name = name;
+	}
+
+	public String getAddress() {
+
+		return address;
+	}
+
+	public void setAddress(String address) {
+
+		this.address = address;
+	}
+
+	public double getLat() {
+
+		return lat;
+	}
+
+	public void setLat(double lat) {
+
+		this.lat = lat;
+	}
+
+	public double getLng() {
+
+		return lng;
+	}
+
+	public void setLng(double lng) {
+
+		this.lng = lng;
+	}
 }
