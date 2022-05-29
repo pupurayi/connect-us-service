359d12061d3c9bd2ac458274756716df8a29c51d
Victor Chinyavada <chinyavadav@gmail.com>
2022-05-29 15:29:48 +0200

    recommendationsMap.html

--- src/main/java/zw/co/connectus/controller/NavigationController.java
+++ src/main/java/zw/co/connectus/controller/NavigationController.java
@@ -1,26 +1,44 @@
 package zw.co.connectus.controller;
 
+import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import zw.co.connectus.service.model.Marker;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/navigation")
 @CrossOrigin(origins = "*")
 public class NavigationController {
 
-	@GetMapping
-	public ModelAndView map(@RequestParam("currentLat") double currentLat,
-	                        @RequestParam("currentLng") double currentLng,
-	                        @RequestParam("destinationLat") double destinationLat,
-	                        @RequestParam("destinationLng") double destinationLng
-	) {
+    @GetMapping
+    public ModelAndView map(@RequestParam("currentLat") double currentLat,
+                            @RequestParam("currentLng") double currentLng,
+                            @RequestParam("destinationLat") double destinationLat,
+                            @RequestParam("destinationLng") double destinationLng
+    ) {
 
-		ModelAndView modelAndView = new ModelAndView("navigationMap");
-		modelAndView.addObject("currentLat", currentLat);
-		modelAndView.addObject("currentLng", currentLng);
-		modelAndView.addObject("destinationLat", destinationLat);
-		modelAndView.addObject("destinationLng", destinationLng);
-		return modelAndView;
-	}
+        ModelAndView modelAndView = new ModelAndView("navigationMap");
+        modelAndView.addObject("currentLat", currentLat);
+        modelAndView.addObject("currentLng", currentLng);
+        modelAndView.addObject("destinationLat", destinationLat);
+        modelAndView.addObject("destinationLng", destinationLng);
+        return modelAndView;
+    }
+
+    @GetMapping("/recommendations")
+    public ModelAndView map(@RequestParam("currentLat") double currentLat,
+                            @RequestParam("currentLng") double currentLng,
+                            @RequestParam("markers") String markers) {
+
+        List<Marker> markerList = new Gson().fromJson(markers, ArrayList.class);
+        ModelAndView modelAndView = new ModelAndView("navigationMap");
+        modelAndView.addObject("currentLat", currentLat);
+        modelAndView.addObject("currentLng", currentLng);
+        modelAndView.addObject("markerList", markerList);
+        return modelAndView;
+    }
 
 }
---
+++ src/main/java/zw/co/connectus/service/model/Marker.java
@@ -0,0 +1,40 @@
+package zw.co.connectus.service.model;
+
+public class Marker {
+    private String productName;
+    private double price;
+    private double lat;
+    private double lng;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+}
