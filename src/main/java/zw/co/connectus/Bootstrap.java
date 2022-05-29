576f823cae0900cf2baa965879e798654d037728
Victor Chinyavada <chinyavadav@gmail.com>
2022-05-29 21:16:41 +0200

    rating

--- src/main/java/zw/co/connectus/Bootstrap.java
+++ src/main/java/zw/co/connectus/Bootstrap.java
@@ -2,8 +2,10 @@ package zw.co.connectus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Bootstrap {
 
 	public static void main(String[] args) {
--- src/main/java/zw/co/connectus/service/RatingServiceImpl.java
+++ src/main/java/zw/co/connectus/service/RatingServiceImpl.java
@@ -3,8 +3,16 @@ package zw.co.connectus.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import zw.co.connectus.dal.entity.Order;
+import zw.co.connectus.dal.entity.Product;
+import zw.co.connectus.dal.entity.UserProductRating;
+import zw.co.connectus.dal.repository.OrderRepository;
 import zw.co.connectus.dal.repository.ProductRepository;
+import zw.co.connectus.dal.repository.UserProductRatingRepository;
+
+import java.util.List;
 
 @Service
 public class RatingServiceImpl {
@@ -14,5 +22,18 @@ public class RatingServiceImpl {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private OrderRepository orderRepository;
 
+    @Autowired
+    private UserProductRatingRepository userProductRatingRepository;
+
+    @Scheduled(fixedDelay = 3600 * 1000)
+    public void scheduledRatingManager() {
+        List<Product> allProducts = productRepository.findAll();
+        List<UserProductRating> allUserProductRatings = userProductRatingRepository.findAll();
+        List<Order> allOrders = orderRepository.findAll();
+        for (Product product : allProducts) {
+        }
+    }
 }
